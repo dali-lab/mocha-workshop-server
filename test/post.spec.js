64481088d0fcefca028078cb5da3b634f8815b72
ijemmao <ijemmao>
2019-02-01 20:04:03 -0500

    finally got before hook and tests

--- test/post.spec.js
+++ test/post.spec.js
@@ -1,13 +1,62 @@
-import { describe, it } from 'mocha';
+import { describe, it, before, after } from 'mocha';
 import { expect } from 'chai';
 import request from 'supertest';
+import User from '../src/models/user';
 import app from '../src/server';
 
+// variables to help with testing
+let token = '';
+let userID = '';
+
 describe('Posts', () => {
+    before((done) => {
+        request(app)
+            .post('/auth/signup')
+            .send({
+                email: 'test@example.com',
+                password: 'password',
+                username: 'test',
+            })
+            .end((err, res) => {
+                expect(res.body.token).to.exist;
+                expect(res.body.user).to.exist;
+                token = res.body.token;
+                userID = res.body.user.id;
+                done();
+            });
+    });
+
+    after((done) => {
+        User.findByIdAndDelete(userID).then(() => {
+            done();
+        });
+    });
+
     describe('#getAll', () => {
-        // create tests here
-        it('should be working out for some odd reason', () => {
-            expect('ijemma').equal('ijemma');
+        it('grab all posts - zero posts', (done) => {
+            request(app)
+                .get('/posts/getPosts')
+                .set({ Authorization: `Bearer ${token}` })
+                .end((err, res) => {
+                    if (err) { done(err); }
+                    expect(res.status).equal(200);
+                    expect(res.body.length).equal(0);
+                    done();
+                });
+        });
+
+        it('grab all posts - 5 posts', (done) => {
+            request(app)
+                .post('/posts/addPost')
+                .set({ Authorization: `Bearer ${token}` })
+                .send({
+                    title: 'This is my first blog post',
+                    body: 'Here is the body of the best blog post ever known to man',
+                    author: userID,
+                })
+                .end((err, res) => {
+                    done();
+                });
         });
     });
 
